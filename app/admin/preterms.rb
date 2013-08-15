9012f86b6ef72b8378b173dc1cf2d2469aad9f3b
Tessa Davis <tessadavis06@gmail.com>
2013-08-15 11:20:59 +1000

    changed active admin display

--- app/admin/preterms.rb
+++ app/admin/preterms.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register Preterm do
   
+   index do                           
+    column :gestation   
+    column :gender               
+    column :steroids       
+    column :singleton
+    column :sga
+    column :nec
+    column :survival
+    column :IVH12
+    column :IVH34
+    column :CLD
+    column :IMV
+    column :homeox
+    column :pda
+    default_actions                   
+  end
 end
